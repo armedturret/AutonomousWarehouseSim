00a6ac816da265e3e869c0e33b5621a451b765c3
Robert White <rjwhitenh@outlook.com>
2022-02-01 10:25:09 -0500

    Added a system to spawn trucks

---
+++ Assets/Scripts/TruckSpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TruckSpawnManager : MonoBehaviour
+{
+    public List<TruckSpawn> truckSpawns;
+
+    public TMP_Dropdown spawnDropdown;
+
+    private string m_mode = "DROPOFF";
+    private string m_spawn = "TRUCKONE";
+    private string m_crates = "";
+
+    public void SetMode(int index)
+    {
+        m_mode = index == 0 ? "DROPOFF" : "PICKUP";
+    }
+
+    public void SetTruckSpawn(int index)
+    {
+        m_spawn = spawnDropdown.options[index].text;
+    }
+
+    public void SetCrates(string crates)
+    {
+        m_crates = crates;
+    }
+
+    public void SpawnTruck()
+    {
+        //spawn a truck at the correct spot with the correct parameters
+        for(int i = 0; i < truckSpawns.Count; i++)
+        {
+            if(truckSpawns[i].location == m_spawn)
+            {
+                truckSpawns[i].SpawnTruck(m_mode + "," + m_spawn + "," + m_crates);
+            }
+        }
+    }
+}
